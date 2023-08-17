bf49bb1801dbbf560f83b5d3010132c4fc560657
Seife1 <gebrealabate19@yahoo.com>
2023-08-17 18:20:26 +0300

    SHOW create table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- full description
 
-SELECT *
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_test_db_5' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE hbtn_test_db_5.first_table;
