8349a25815e2ccff261e9f58633bb2358b262203
Seife1 <gebrealabate19@yahoo.com>
2023-08-18 15:00:31 +0300

    DB as arg

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- A script that lists all the cities of California
 
-USE hbtn_0d_usa;
 
 SELECT id, name
 FROM cities AS c 
--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- A script that lists all cities contained in the DATABASE
 
-USE hbtn_0d_usa;
 SELECT c.id, c.name, s.name
 FROM cities AS c NATURAL JOIN states AS s
 ORDER BY c.id;
